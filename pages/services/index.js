fe290b6debfc0f9d3a244baf92714054ba6873a5
AngryPacifist <meldiablo23@gmail.com>
2024-05-31 01:12:46 +0100

    more portfolio updates

--- pages/services/index.js
+++ pages/services/index.js
@@ -58,7 +58,7 @@ const Services = () => {
     <motion.h2 variants={fadeIn('up' , '0.2')} 
     initial='hidden' 
     animate='show' 
-    exit='hidden'  className=" h2 xl:mt-8">My Portfolio <span className=" text-accent">.</span></motion.h2>
+    exit='hidden'  className=" h2 xl:mt-8">My Portfolio<span className=" text-accent">.</span></motion.h2>
     <motion.p variants={fadeIn('up' , '0.4')} 
     initial='hidden' 
     animate='show' 
